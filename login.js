4d854fdef6948a574953e98a046ef3b76db0a1e4
Cavey <m.123051427@gmail.com>
2024-05-13 20:51:56 +0800

    feat: add auto login 1. migrate to manifest v3 2. add auto login feature

--- login.js
+++ login.js
@@ -1,2 +1,25 @@
+let account, password;
+
+function getUser() {
+    return new Promise(resolve => {
+        chrome.storage.sync.get(['account', 'password'], items => {
+            account = items.account;
+            password = items.password;
+            resolve();
+        })
+    });
+}
+
 if (document.getElementById('userNameOverflow').textContent.trim() != "Login")
     chrome.runtime.sendMessage({ greeting: 'startQuiz' });
+else {
+    getUser().then(()=>{
+        document.getElementById('loginUserName').value=account;
+        document.getElementById('loginPassword').value=password;
+        let loginBtn=document.getElementById('wizardLoginButton');
+        loginBtn=loginBtn.getElementsByTagName('input')[0];
+        loginBtn.click();
+        console.log(loginBtn);
+    })
+
+}
